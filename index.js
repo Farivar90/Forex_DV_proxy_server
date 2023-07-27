2ff1b808e0f0e54691878ec84bbf78764fd40b04
farivar <farivaramiri@gmail.com>
2023-07-26 21:40:30 -0700

    fix some server problem

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 
+
 if (process.env.NODE_ENV === 'development') {
   const dotenv = require('dotenv');
   dotenv.config();
@@ -17,6 +18,20 @@ app.get('/', async (req, res) => {
     res.send(proxyRes.data);
   } else {
     res.send('Please provide a url query parameter');
+  }
+});
+
+app.get('/', async (req, res) => {
+  try {
+    if (req.query.url) {
+      const proxyRes = await axios.get(req.query.url);
+      res.send(proxyRes.data);
+    } else {
+      res.status(400).send('Please provide a url query parameter');
+    }
+  } catch (error) {
+    console.error('Error fetching data from the API:', error);
+    res.status(500).send('An error occurred while fetching data');
   }
 });
 
